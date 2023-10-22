fbbe07b67dcb5924b453f1ff4d239222d08ebce4
Jamie <70950884+JamieVo890@users.noreply.github.com>
2023-10-22 22:26:30 +0800

    Update Align Images.sh

--- Image Processing/Align Images.sh
+++ Image Processing/Align Images.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Specify the directory where your folders are located
+# This script is used to run FSL flirt to align images to MNI space. 
+
+# Directory of PIB PET images
 directory="/mnt/hgfs/work/Processed_PIB/3.Normalise"
+# Directory of MNI template
 standard="/home/jamie/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz"
-# Specify the output directory where aligned images will be saved
+# MNI aligned images output directory
 output_directory="/mnt/hgfs/work/Processed_PIB/4.MNI_Registered"
 
-# Iterate over files in the input directory
+
 for file in "$directory"/*; do
-    # Extract file name without the path
     filename=$(basename "$file")
 
-    # Extract file extension
     extension="${filename##*.}"
 
-    # Remove extension from filename
     filename_no_ext="${filename%.*}"
 
     flirt -in "$file" -ref $standard -cost corratio -searchcost corratio -out "$output_directory/${filename_no_ext}.$extension"
