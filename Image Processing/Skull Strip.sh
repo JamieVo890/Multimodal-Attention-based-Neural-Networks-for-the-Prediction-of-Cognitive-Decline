279f9eea6a650d5b4b12973601593f7fac08e794
Jamie <70950884+JamieVo890@users.noreply.github.com>
2023-10-22 22:29:57 +0800

    Update Skull Strip.sh

--- Image Processing/Skull Strip.sh
+++ Image Processing/Skull Strip.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script is used to strip the skull and noise from PIB images.
+# It iterates over images in a directory and runs BET and  mri_synthstrip to remove skull and noise.
 
 # Specify input and output directories
 input_dir="/mnt/hgfs/work/Processed_PIB/1.Mean_AVG_Smooth"
@@ -7,18 +9,16 @@ output_dir2="/mnt/hgfs/work/Processed_PIB/2.Skull_Strip/2.FINAL"
 
 # Iterate over files in the input directory
 for file in "$input_dir"/*; do
-    # Extract file name without the path
+
     filename=$(basename "$file")
 
-    # Extract file extension
     extension="${filename##*.}"
 
-    # Remove extension from filename
     filename_no_ext="${filename%.*}"
 
-    # Run bet command
+    # Run BET
     bet "$file" "$output_dir1/${filename_no_ext}.$extension" -f 0.5 -g 0.1
 
-    # Run mri_synthstrip command
+    # Run mri_synthstrip
     mri_synthstrip -i "$output_dir1/${filename_no_ext}.$extension" -o "$output_dir2/${filename_no_ext}.$extension" -b 4
 done
